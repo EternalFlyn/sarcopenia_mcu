92236e460f33e4a6ab920f1948c08a5dedc9cc9e
EternalFlyn <EternalFlyn@gmail.com>
2023-02-10 17:09:46 +0800

    [0.3.1f] - 2023-02-10 changed - the ADC channel increase to 6 for the foot pressure sensor

--- src/adc/adc.c
+++ src/adc/adc.c
@@ -9,40 +9,92 @@
 #include "adc.h"
 #include "adc_service.h"
 
-#define ADC_SAMPLE_TIME_US 900
+#define ADC_SAMPLE_TIME_US 100*1000 // 10Hz
 
 #define EXTRA_SAMPLING 0
-#define CHANNEL_AMOUNT 2
+#define CHANNEL_AMOUNT 5
 #define BUFFER_SIZE CHANNEL_AMOUNT*(1+EXTRA_SAMPLING)
-#define ADC_GAIN ADC_GAIN_1_4
-#define ADC_REFERENCE ADC_REF_VDD_1_4
+#define ADC_GAIN ADC_GAIN_1
+#define ADC_REFERENCE ADC_REF_VDD_1
 #define ADC_ACQUISITION_TIME ADC_ACQ_TIME(ADC_ACQ_TIME_MICROSECONDS, 10)
 #define ADC_RESOLUTION 12
 
-#define ADC_LEFT_CHANNEL_ID 0
-#define ADC_LEFT_CHANNEL_INPUT NRF_SAADC_INPUT_AIN0
-#define ADC_RIGHT_CHANNEL_ID 1
-#define ADC_RIGHT_CHANNEL_INPUT NRF_SAADC_INPUT_AIN1
+//定義channel0~5之AIN腳位
+//Sparkfun nrf52840 mini 使用 A0、A1、A7、A6、A5、A4
+//nrf52840DK 使用 A1、A2、A4、A5、A6、A7
+//nrf5340DK 使用 A0、A1、A2、A3、A4、A5
+#define ADC_1ST_CHANNEL_ID 0
+#define ADC_1ST_CHANNEL_INPUT NRF_SAADC_INPUT_AIN0
+#define ADC_2ND_CHANNEL_ID 1
+#define ADC_2ND_CHANNEL_INPUT NRF_SAADC_INPUT_AIN1
+#define ADC_3RD_CHANNEL_ID 4
+#define ADC_3RD_CHANNEL_INPUT NRF_SAADC_INPUT_AIN7
+#define ADC_4TH_CHANNEL_ID 5
+#define ADC_4TH_CHANNEL_INPUT NRF_SAADC_INPUT_AIN6
+#define ADC_5TH_CHANNEL_ID 2
+#define ADC_5TH_CHANNEL_INPUT NRF_SAADC_INPUT_AIN5
+#define ADC_6TH_CHANNEL_ID 3
+#define ADC_6TH_CHANNEL_INPUT NRF_SAADC_INPUT_AIN4
 
 static int16_t sampling_buffer[BUFFER_SIZE];
 
-static const struct adc_channel_cfg emg_left_channel_cfg = {
+static const struct adc_channel_cfg adc1_channel_cfg = {
 	.gain = ADC_GAIN,
 	.reference = ADC_REFERENCE,
 	.acquisition_time = ADC_ACQUISITION_TIME,
-	.channel_id = ADC_LEFT_CHANNEL_ID,
+	.channel_id = ADC_1ST_CHANNEL_ID,
 #if defined(CONFIG_ADC_CONFIGURABLE_INPUTS)
-	.input_positive = ADC_LEFT_CHANNEL_INPUT,
+	.input_positive = ADC_1ST_CHANNEL_INPUT,
 #endif
 };
 
-static const struct adc_channel_cfg emg_right_channel_cfg = {
+static const struct adc_channel_cfg adc2_channel_cfg = {
 	.gain = ADC_GAIN,
 	.reference = ADC_REFERENCE,
 	.acquisition_time = ADC_ACQUISITION_TIME,
-	.channel_id = ADC_RIGHT_CHANNEL_ID,
+	.channel_id = ADC_2ND_CHANNEL_ID,
 #if defined(CONFIG_ADC_CONFIGURABLE_INPUTS)
-	.input_positive = ADC_RIGHT_CHANNEL_INPUT,
+	.input_positive = ADC_2ND_CHANNEL_INPUT,
+#endif
+};
+
+static const struct adc_channel_cfg adc3_channel_cfg = {
+	.gain = ADC_GAIN,
+	.reference = ADC_REFERENCE,
+	.acquisition_time = ADC_ACQUISITION_TIME,
+	.channel_id = ADC_3RD_CHANNEL_ID,
+#if defined(CONFIG_ADC_CONFIGURABLE_INPUTS)
+	.input_positive = ADC_3RD_CHANNEL_INPUT,
+#endif
+};
+
+static const struct adc_channel_cfg adc4_channel_cfg = {
+	.gain = ADC_GAIN,
+	.reference = ADC_REFERENCE,
+	.acquisition_time = ADC_ACQUISITION_TIME,
+	.channel_id = ADC_4TH_CHANNEL_ID,
+#if defined(CONFIG_ADC_CONFIGURABLE_INPUTS)
+	.input_positive = ADC_4TH_CHANNEL_INPUT,
+#endif
+};
+
+static const struct adc_channel_cfg adc5_channel_cfg = {
+	.gain = ADC_GAIN,
+	.reference = ADC_REFERENCE,
+	.acquisition_time = ADC_ACQUISITION_TIME,
+	.channel_id = ADC_5TH_CHANNEL_ID,
+#if defined(CONFIG_ADC_CONFIGURABLE_INPUTS)
+	.input_positive = ADC_5TH_CHANNEL_INPUT,
+#endif
+};
+
+static const struct adc_channel_cfg adc6_channel_cfg = {
+	.gain = ADC_GAIN,
+	.reference = ADC_REFERENCE,
+	.acquisition_time = ADC_ACQUISITION_TIME,
+	.channel_id = ADC_6TH_CHANNEL_ID,
+#if defined(CONFIG_ADC_CONFIGURABLE_INPUTS)
+	.input_positive = ADC_6TH_CHANNEL_INPUT,
 #endif
 };
 
@@ -74,8 +126,12 @@ int adc_init(const struct device *dev) {
         printk("ADC: Device is not ready.\n");
         return -1;
     }
-	ret = adc_channel_setup(dev, &emg_left_channel_cfg);
-	ret = adc_channel_setup(dev, &emg_right_channel_cfg);
+	ret = adc_channel_setup(dev, &adc1_channel_cfg);
+	ret = adc_channel_setup(dev, &adc2_channel_cfg);
+	ret = adc_channel_setup(dev, &adc3_channel_cfg);
+	ret = adc_channel_setup(dev, &adc4_channel_cfg);
+	ret = adc_channel_setup(dev, &adc5_channel_cfg);
+	ret = adc_channel_setup(dev, &adc6_channel_cfg);
 	if (ret) {
 		printk("Error in adc setup: %d\n", ret);
         return ret;
@@ -95,7 +151,8 @@ int adc_sample(const struct device *dev) {
 
 	const struct adc_sequence sequence = {
 		.options = &sequence_opts,
-		.channels = BIT(ADC_LEFT_CHANNEL_ID) | BIT(ADC_RIGHT_CHANNEL_ID),
+		.channels = BIT(ADC_1ST_CHANNEL_ID) | BIT(ADC_2ND_CHANNEL_ID) | BIT(ADC_3RD_CHANNEL_ID)
+		          | BIT(ADC_4TH_CHANNEL_ID) | BIT(ADC_5TH_CHANNEL_ID) | BIT(ADC_6TH_CHANNEL_ID),
 		.buffer = sampling_buffer,
 		.buffer_size = sizeof(sampling_buffer),
 		.resolution = ADC_RESOLUTION,
