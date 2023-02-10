da97094d969c65b77b41c3d32f7ce062101dd45d
EternalFlyn <EternalFlyn@gmail.com>
2023-02-10 16:50:49 +0800

    [0.3.1f] - 2023-02-10 removed - code of related i2c function

--- src/main.c
+++ src/main.c
@@ -13,10 +13,6 @@
 #include "flyn_bluetooth.h"
 #include "adc.h"
 #include "adc_service.h"
-#include "mpu6050.h"
-#include "mpu6050_service.h"
-
-#define IMU_SAMPLE_TIME 100
 
 #define ADC_DEVICE_NAME DT_LABEL(DT_INST(0, nordic_nrf_saadc))
 
@@ -24,31 +20,7 @@
 #define THREAD_PRIORITY 5
 
 const struct device *adc_dev;
-const struct device *i2c_dev;
 const struct device *gpio0_dev;
-static struct mpu6050_data imu_data;
-
-void imu_sample_event() {
-	int err = 0;
-	int cnt = 0;
-	while (true) {
-		err = mpu6050_sample(i2c_dev, &imu_data);
-		if (err) {
-			printk("Error in i2c sampling: %d\n", err);
-			return;
-		}
-		imu_data_update(imu_data.ax, imu_data.ay, imu_data.az, imu_data.gx, imu_data.gy, imu_data.gz);
-		
-		accelerometer_notify();
-		gyroscope_notify();
-
-		// printk("imu sample at: %d\n", k_cyc_to_us_near32(k_cycle_get_32()));
-
-		// printk("ax: %d, ay: %d, az: %d\n", imu_data.ax, imu_data.ay, imu_data.az);
-		// printk("gx: %d, gy: %d, gz: %d\n", imu_data.gx, imu_data.gy, imu_data.gz);
-		k_msleep(IMU_SAMPLE_TIME);
-	}
-}
 
 void main(void) {
 	printk("Hello World! %s\n", CONFIG_BOARD);
@@ -56,10 +28,6 @@ void main(void) {
 	// ADC setup
     adc_dev = device_get_binding(ADC_DEVICE_NAME);
 	if (adc_init(adc_dev)) return;
-
-	// I2C setup
-	i2c_dev = DEVICE_DT_GET(DT_NODELABEL(i2c1));
-	if (mpu6050_init(i2c_dev)) return;
 
 	if (bt_init()) return;
 
@@ -69,5 +37,3 @@ void main(void) {
 
 	adc_sample(adc_dev);
 }
-
-K_THREAD_DEFINE(imu_sample_thread, THREAD_STACK_SIZE, imu_sample_event, NULL, NULL, NULL, THREAD_PRIORITY, 0, 0);
