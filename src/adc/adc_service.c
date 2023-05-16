ada85f909691f2d90ca4f4984893476e36c403c1
EternalFlyn <EternalFlyn@gmail.com>
2023-05-16 15:18:57 +0800

    [0.3.2f] - 2023-05-16 added - queue of foot pressure data changed - package the foot pressure data

--- src/adc/adc_service.c
+++ src/adc/adc_service.c
@@ -3,12 +3,22 @@
 #include <stdlib.h>
 
 #include "adc_service.h"
+#include "foot_pressure_queue.h"
+
+#define DATA_AMOUNT_PER_PACKAGE 25
+
+K_QUEUE_DEFINE(FOOT_PRESSURE_QUEUE);
 
 static struct bt_uuid_128 adc_uuid = BT_UUID_INIT_128(ADC_SERVICE_UUID_VAL);
 static struct bt_uuid_128 foot_pressure_uuid = BT_UUID_INIT_128(BT_UUID_128_ENCODE(0xD16F7A3D, 0x1897, 0x40EA, 0x9629, 0xBDF749AC5991));
 
-static int16_t raw_array[6];
 static uint8_t notify_flag;
+
+static uint8_t package_num = 0;
+static uint8_t sampling_count = 0;
+
+static foot_pressure_data_t raw_array[DATA_AMOUNT_PER_PACKAGE];
+static uint8_t buffer[247];
 
 static void foot_pressure_ccc_cfg_changed(const struct bt_gatt_attr *attr, uint16_t value) {
 	notify_flag = (value == BT_GATT_CCC_NOTIFY) ? 1 : 0;
@@ -22,10 +32,42 @@ BT_GATT_SERVICE_DEFINE(adc_service,
 
 uint32_t prev_us_time = 0;
 
+uint16_t encode(struct k_queue* queue) {
+    uint16_t amount = adc_queue_pop_amount(queue, raw_array, DATA_AMOUNT_PER_PACKAGE);
+    printk("Amount: %d\n", amount);
+    // calculate buffer size 
+    // package_number(1 byte) | list_size(1 byte) | data...(x) | check_sum(1 byte)
+    uint16_t buffer_len = 3;
+    buffer_len += amount * 12 / 8;
+    if (amount % 2) buffer_len++;
+
+    // add data size to head
+    buffer[0] = package_num;
+    buffer[1] = amount;
+
+    // put data into buffer
+    uint16_t loc = 2;
+    for (int i = 0; i < amount; i++) {
+        for (int j = 0; j < 6; j++) {
+            uint8_t hi_bit = (raw_array[i].value[j] >> 8) & 0x0F;
+            uint8_t lo_bit = raw_array[i].value[j] & 0xFF;
+            if (i % 2) {
+                buffer[loc] |= hi_bit;
+                buffer[loc + 2] = lo_bit;
+                loc += 3;
+            }
+            else {
+                buffer[loc] = hi_bit << 4;
+                buffer[loc + 1] = lo_bit;
+            }
+        }
+    }
+}
+
 void adc_raw_notify() {
     if (!notify_flag) return;
-
-    bt_gatt_notify(NULL, &adc_service.attrs[1], raw_array, 6);
+    uint16_t len = encode(&FOOT_PRESSURE_QUEUE);
+    bt_gatt_notify(NULL, &adc_service.attrs[1], buffer, len);
 
     // measure notification time interval
     uint32_t time = k_cyc_to_us_near32(k_cycle_get_32());
@@ -35,12 +77,16 @@ void adc_raw_notify() {
 
 void adc_data_update(int16_t ha, int16_t lt, int16_t m1, int16_t m5, int16_t arch, int16_t hm) {
     if (!notify_flag) return;
+
+    sampling_count++;
+
+    foot_pressure_data_t data = {
+        .value = {ha, lt, m1, m5, arch, hm}
+    };
+    foot_pressure_queue_push(&FOOT_PRESSURE_QUEUE, data);
     
-    raw_array[0] = ha;
-    raw_array[1] = lt;
-    raw_array[2] = m1;
-    raw_array[3] = m5;
-    raw_array[4] = arch;
-    raw_array[5] = hm;
-    adc_raw_notify();
+    if (sampling_count == DATA_AMOUNT_PER_PACKAGE) {
+        adc_raw_notify();
+        sampling_count = 0;
+    }
 }
--- src/adc/foot_pressure_queue.c
+++ src/adc/foot_pressure_queue.c
@@ -2,41 +2,45 @@
 #include <stdint.h>
 #include <kernel.h>
 
-#include "adc_queue.h"
+#include "foot_pressure_queue.h"
 
 #define QUEUE_BLOCK_SIZE 8
 #define QUEUE_BLOCK_COUNT 1024
 
 K_MEM_SLAB_DEFINE(queue_slab, QUEUE_BLOCK_SIZE, QUEUE_BLOCK_COUNT, sizeof(void *));
 
-void adc_queue_push(struct k_queue *queue, int16_t value) {
-    adc_node_t *node;
+void foot_pressure_queue_push(struct k_queue *queue, foot_pressure_data_t data) {
+    data_node_t *node;
     if (!k_mem_slab_alloc(&queue_slab, &node, K_NO_WAIT)) {
-        node->value = value;
+        node->data.value[0] = data.value[0];
+        node->data.value[1] = data.value[1];
+        node->data.value[2] = data.value[2];
+        node->data.value[3] = data.value[3];
+        node->data.value[4] = data.value[4];
+        node->data.value[5] = data.value[5];
     }
     else {
         printk("Memory allocation fail\n");
         return;
     }
-
     k_queue_append(queue, (void *) node);
 }
 
-uint16_t adc_queue_pop_amount(struct k_queue *queue, int16_t *array, uint16_t amount) {
+uint16_t foot_pressure_queue_pop_amount(struct k_queue *queue, foot_pressure_data_t *array, uint16_t amount) {
     uint16_t pop_amount = 0;
-    adc_node_t *node;
+    data_node_t *node;
     for (int i = 0; i < amount; i++) {
         if (k_queue_is_empty(queue)) break;
         node = k_queue_get(queue, K_NO_WAIT);
-        array[i] = node->value;
+        array[i] = node->data;
         k_mem_slab_free(&queue_slab, &node);
         pop_amount++;
     }
     return pop_amount;
 }
 
-void adc_queue_clean(struct k_queue *queue) {
-    adc_node_t *node;
+void foot_pressure_queue_clean(struct k_queue *queue) {
+    data_node_t *node;
     while (!k_queue_is_empty(queue)) {
         node = k_queue_get(queue, K_NO_WAIT);
         k_mem_slab_free(&queue_slab, &node);
