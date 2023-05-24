f94ead264fcb10febb8f2d4dcf855ea87808b4cc
EternalFlyn <EternalFlyn@gmail.com>
2023-05-24 13:57:11 +0800

    [0.3.2f] - 2023-05-24 added - use the fixed BLE address

--- src/bluetooth/flyn_bluetooth.c
+++ src/bluetooth/flyn_bluetooth.c
@@ -4,6 +4,7 @@
 #include <settings/settings.h>
 
 #include <bluetooth/bluetooth.h>
+#include <bluetooth/addr.h>
 #include <bluetooth/conn.h>
 #include <bluetooth/gatt.h>
 
@@ -21,12 +22,45 @@ static const struct bt_data sd[] = {
 	BT_DATA_BYTES(BT_DATA_UUID128_ALL, ADC_SERVICE_UUID_VAL)
 };
 
+static uint8_t set_bt_addr(const char *addr_str) {
+	uint8_t err;
+	bt_addr_le_t addr;
+
+	err = bt_addr_le_from_str(addr_str, "random", &addr);
+	if (err) {
+		printk("Invalid BT address (err %d)\n", err);
+	}
+
+	err = bt_id_create(&addr, NULL);
+	if (err < 0) {
+		printk("Creating new ID failed (err %d)\n", err);
+		return 0;
+	}
+	printk("new addr: %02x:%02x:%02x:%02x:%02x:%02x\n", addr.a.val[5], addr.a.val[4], addr.a.val[3], addr.a.val[2], addr.a.val[1], addr.a.val[0]);
+	return err;
+}
+
 static int bt_ready() {
 	int err;
 
+	struct bt_le_adv_param adv_param = {
+		.id = BT_ID_DEFAULT,
+		.sid = 0,
+		.secondary_max_skip = 0,
+		.options = (BT_LE_ADV_OPT_CONNECTABLE |
+			    BT_LE_ADV_OPT_USE_NAME |
+			    BT_LE_ADV_OPT_ONE_TIME |
+				BT_LE_ADV_OPT_USE_IDENTITY),
+		.interval_min = 0x00a0, /* 100 ms */
+		.interval_max = 0x00f0, /* 150 ms */
+		.peer = NULL,
+	};
+
+	// adv_param.id = set_bt_addr("E0:00:49:00:00:02");
+
 	printk("Bluetooth initialized\n");
 	
-	err = bt_le_adv_start(BT_LE_ADV_CONN_NAME, ad, ARRAY_SIZE(ad), sd, 0);
+	err = bt_le_adv_start(&adv_param, ad, ARRAY_SIZE(ad), sd, 0);
 	if (err) {
 		printk("Advertising failed to start (err %d)\n", err);
 		return err;
